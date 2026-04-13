c935f890ff66dd39c6194e42ae9c19ad9af358d0
j-haneul <j.haneul@skhappiness.org>
2026-04-13 17:02:00 +0900

    Update data.js

--- public/data.js
+++ public/data.js
@@ -1,23 +1,23 @@
 const SUNNY_DATA = [
     {
         id: "fore-1",
-        teamId: "fo:re",
+        teamId: "fore",
         teamOrder: "01",
         teamName: "포레 : 조부모 돌봄 자립준비청년",
         part: "PART 01",
         title: "문제의 실마리: 조부모라는 변수",
-        thumbnail: "foret_1.png", // 실제 이미지 파일명
-        link: "article_view.html?id=foret-1"
+        thumbnail: "fore_1.png", // 실제 이미지 파일명
+        link: "article_view.html?id=fore-1"
     },
     {
         id: "fore-2",
-        teamId: "fo:re",
+        teamId: "fore",
         teamOrder: "01",
         teamName: "포레 : 조부모 돌봄 자립준비청년",
         part: "PART 02",
         title: "현장의 목소리: 숨겨진 돌봄의 무게",
-        thumbnail: "foret_2.png",
-        link: "article_view.html?id=foret-2"
+        thumbnail: "fore_2.png",
+        link: "article_view.html?id=fore-2"
     },
     {
         id: "green-1",
