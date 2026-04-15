0308793213c3acc3b881a14449b153a99d804c72
j-haneul <j.haneul@skhappiness.org>
2026-04-15 15:57:34 +0900

    Update data.js

--- public/data.js
+++ public/data.js
@@ -55,7 +55,7 @@ const TEAM_DETAILS = {
     "fore": {
         order: "LAB 01",
         name: "포레(fo:re)",
-        teamProfileImg: "images/fore_team_1.jpg", 
+        teamImg: "images/fore_team_1.jpg", 
         description: "저희 fou:re(포레)는 사회를 바라볼 때 당연하게 여겨져 온 것들을 다시 질문하는 데서 출발합니다. 익숙함 속에서 무심히 지나쳐 온 문제를 되짚으며, 사회를 서로 연결된 하나의 숲(forest)처럼  바라봅니다. 동시에 four(4명)과 re(다시·회복)의 결합으로, 실패하더라도 끊임없이 다시 도전하고 시도하겠다는 의지를 담고 있습니다.",
         email: "fore_sunny@example.com",
         sns: "@fore_lab_official",
@@ -69,7 +69,7 @@ const TEAM_DETAILS = {
     "green": {
         order: "LAB 02",
         name: "그린 : 자립준비청년 학업 중단",
-        teamProfileImg: "public/images/team_green_logo.png", 
+        teamProfileImg: "", 
         description: "자립준비청년들이 경제적 어려움으로 인해 학업을 포기하지 않도록, 지속 가능한 교육 환경과 정책적 지원 방안을 연구합니다.",
         email: "green_sunny@example.com",
         sns: "@green_scholar",
