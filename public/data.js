ae64e989b58b4fcb45b75f6865ba6b628ab59da0
j-haneul <j.haneul@skhappiness.org>
2026-04-15 15:07:50 +0900

    Update data.js

--- public/data.js
+++ public/data.js
@@ -46,6 +46,7 @@ const SUNNY_DATA = [
         teamName: "포레 (FORE)",
         part: "Part 2. 원인 분석",
         title: "보이지 않는 돌봄의 무게: 통계로 본 현실",
+ thumbnail: "images/fore_2.png",
         content: "<p>두 번째 파트 내용이 들어갈 자리입니다.</p>"
     }
 ];
